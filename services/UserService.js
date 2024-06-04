49104e5b69d37709e6da89ac651c747639aea873
Ege Moroğlu <egemoroglu@hotmail.com>
2024-06-04 14:51:23 +0300

    UserService and TodoService

--- services/UserService.js
+++ services/UserService.js
@@ -2,7 +2,6 @@ const db = require('../model/database');
 
 class UserService {
 
-    //add user function that calls the addUser function in the database
     async addUser(username, password) {
         return new Promise((resolve, reject) => {
             db.addUser(username, password, function(err, result) {
@@ -15,7 +14,6 @@ class UserService {
         });
     }
 
-    //find user function that calls the findUser function in the database
     async findUser(username, password) {
         return new Promise((resolve, reject) => {
             db.findUser(username, password, function(err, result) {
@@ -28,7 +26,6 @@ class UserService {
         });
     }
 
-    //find username function that calls the findUsername function in the database
     async findUsername(username) {
         return new Promise((resolve, reject) => {
             db.findUsername(username, function(err, result) {
