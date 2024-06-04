9637e68f7130c36ed7c4aff150f34e407f038580
Ege Moroğlu <egemoroglu@hotmail.com>
2024-06-04 14:22:33 +0300

    minor changes in database connection and gitignore

--- model/connection.js
+++ model/connection.js
@@ -1,10 +1,10 @@
 const {Client} = require('pg');
 
 const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'ClientSide',
-    password: 'Gktrk06.',
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
     port: 5432
 });
 
